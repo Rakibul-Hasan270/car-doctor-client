e94b4b9bc2f3b90fdfa223980059bdc6425da9e2
Rakibul-Hasan270 <rakibx270@gmail.com>
2025-03-22 16:15:46 +0600

    added banner with full responcive,bg-gradient and text

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
         <li><NavLink to='/contact'>Contact</NavLink></li>
     </div>
     return (
-        <div className="navbar bg-base-100 shadow-sm mt-10">
+        <div className="navbar bg-base-100 shadow-sm">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
